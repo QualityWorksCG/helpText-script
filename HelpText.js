eb59d93b28f13b6bb54d226ff26b8d49504e0853
Duran Thomas <duran.d.thomas@outlook.com>
2021-04-27 20:56:21 -0500

    Add Script File

---
+++ HelpText.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+
+var AWS = require("aws-sdk");
+// Set the region here
+AWS.config.update({ region: "us-east-1" });
+
+// Create DynamoDB service object
+var ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+
+const papa = require('papaparse')
+
+//Set CSV file location here
+var file = fs.createReadStream('./CMMC Help Text - Level 3c.csv')
+var csvData = []
+
+var results = papa.parse(file,{
+  header: true,
+  dynamicTyping: true,
+  download:true,
+  step: function(result) {
+
+    csvData.push({
+      PutRequest:{ 
+        Item: {
+          frameworkId: {
+            S: String(result.data.frameworkId)
+          },
+          sectionId: {
+            S: result.data.sectionId
+          },
+          controlId: {
+            S:  result.data.controlId
+          },
+          data: {
+            S: result.data.data
+          },
+          referenceId: {
+            S: result.data.referenceId 
+          },
+          helpText: {
+            S: result.data.helpText
+          }
+        } 
+      }
+    })
+
+  },
+  complete: function(results, file) {
+    const _results = {RequestItems: {
+      HelpText: [
+        ...csvData
+      ]
+    }}
+
+    ddb.batchWriteItem(_results, function (err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data);
+      }
+    });
+  }
+});
